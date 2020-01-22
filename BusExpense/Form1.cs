31686a779752d5332cb3784ce5d28096291aee66
alicia65 <alicia65>
2020-01-22 15:00:57 -0600

    Add project files.

---
+++ BusExpense/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusExpense
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTrips_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click_1(object sender, EventArgs e)
+        {
+            double busFare = 2.0;
+
+            int trips;
+
+            if (Int32.TryParse(txtTrips.Text, out trips)) { 
+                double total = busFare * trips;
+                txtTotal.Text = "$" + total;
+            }
+            else
+            {
+                MessageBox.Show("Enter only number", "Error");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
