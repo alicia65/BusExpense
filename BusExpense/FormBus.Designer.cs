eec8fa740283a11c08c936436b4066a6ed249f2b
alicia65 <alicia65>
2020-01-23 10:29:00 -0600

    change TabIndexes

--- BusExpense/FormBus.Designer.cs
+++ BusExpense/FormBus.Designer.cs
@@ -93,7 +93,6 @@
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(155, 42);
             this.btnExit.TabIndex = 2;
-            this.btnExit.TabStop = false;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
